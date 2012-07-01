5a6581393e776180364c8b6bed17bf7b733d9f70
logsol <fartman@gmx.de>
2012-07-01 15:16:33 +0200

    repaired debug draw, added more classes from game draft

--- server.js
+++ server.js
@@ -19,28 +19,30 @@ requirejs(["Chuck/Chuck"], function(Chuck) {
 var fileServer = new nodeStatic.Server('./');
 var server = http.createServer(
 	function(req, res){
-		switch(req.url) {
-			case '/':
-				fileServer.serveFile('./index.html', 200, {}, req, res);
-				break;
+		req.addListener('end', function () {
+			switch(req.url) {
+				case '/':
+					fileServer.serveFile('./index.html', 200, {}, req, res);
+					break;
 
-			case '/client.js':
-				fileServer.serveFile('./client.js', 200, {}, req, res);
-				break;
+				case '/client.js':
+					fileServer.serveFile('./client.js', 200, {}, req, res);
+					break;
 
-			case '/require.js':
-				fileServer.serveFile('./node_modules/requirejs/require.js', 200, {}, req, res);
-				break;
+				case '/require.js':
+					fileServer.serveFile('./node_modules/requirejs/require.js', 200, {}, req, res);
+					break;
 
-			default:
-				if(req.url.match(/^\/lib/)) {
-					fileServer.serve(req, res);
-				} else {
-					res.writeHead(404, {'Content-Type': 'text/html'}); 
-					res.end('<h1>404 not ... found</h1>'); 
-				}
-				break;
-		}
+				default:
+					if(req.url.match(/^\/lib/)) {
+						fileServer.serve(req, res);
+					} else {
+						res.writeHead(404, {'Content-Type': 'text/html'}); 
+						res.end('<h1>404 not ... found</h1>'); 
+					}
+					break;
+			}
+		});
 	}
 );
 server.listen(1234);
