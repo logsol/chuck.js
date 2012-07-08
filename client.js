81d2aa4ddc932f3a0875db5bbacaf045d190b653
logsol <fartman@gmx.de>
2012-07-08 04:29:14 +0200

    extended client server communication

--- client.js
+++ client.js
@@ -2,50 +2,11 @@ requirejs.config({
 	baseUrl: 'lib'
 });
 
-var Chuck;
-requirejs(["Chuck/Chuck"], function(c) {
-	Chuck = c;
-	setupSocket();
-});
+var inspector = {};
 
-function setupSocket(){
+requirejs(["Client/Networker"], function(Networker) {
 	var socket = io.connect(location.href);
+	var networker = new Networker(socket);
 
-	socket.on('connect', onConnect);
-	socket.on('message', onMessage);
-	socket.on('disconnect',onDisconnect);
-}
-
-function onConnect () {
-	console.log('Client connected');
-	Chuck.init();
-}
-
-function onMessage (message) {
-	var commands = JSON.parse(message);
-		
-	for(var command in commands) {
-		processControlCommand(type, command[type]);
-	}
-}
-
-function onDisconnect () {
-	console.log('client disconnected');
-}
-
-function processControlCommand(command, options){
-	switch(command) {
-		case 'joined':
-			break;
-
-		case 'nick':
-			break;
-
-		case 'gameCommand':
-			Chuck.processGameCommand(options);
-			break;
-
-		default:	
-			break;
-	}
-}
+	inspector.networker = networker;
+});
