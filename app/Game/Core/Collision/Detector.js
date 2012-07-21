9c49eccab4debab64874e71724988547f2c5114a
Jeena Paradies <spam@jeenaparadies.net>
2012-07-21 21:52:40 +0200

    Added a constant

---
+++ app/Game/Core/Collision/Detector.js
@@ -0,0 +1,14 @@
+define(function() {
+
+    function Detector() {
+    }
+
+    Detecotr.COLLISION_IDENTIFIER_TILE = "tile";
+
+    Detector.prototype.name = function(arguments){
+        return null;
+    }
+
+    return Detector;
+
+});
